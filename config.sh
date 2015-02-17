6d7308e6136ebcbd2bfd54b025fd80dc1adb219a
Ian Maddison <ian@i.need.stronger.coffee>
2015-02-17 11:42:36 +0000

    Add a config script to set env vars

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+export MONGO_HOST="localhost"
+export MONGO_PORT="27017"
+export GOCARDLESS_USER="<your client key here>"
+export GOCARDLESS_PASS="<your secret here>"
+export STRIPE_KEY="<your key here>"
+export PAYPAL_USER="<your user here>"
+export PAYPAL_PASS="<your pass here>"
+export EMAIL_USER="<your user here>"
+export EMAIL_PASS="<your pass here>"
